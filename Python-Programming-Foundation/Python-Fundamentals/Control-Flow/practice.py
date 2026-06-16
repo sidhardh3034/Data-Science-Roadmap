da5cc259a9d80d04ca83a32559bf3f342f35585a
sidhardh3034 <sidhardh3034@gmail.com>
2026-06-16 16:54:23 +0530

    Complete Control Flow

--- Python-Programming-Foundation/Python-Fundamentals/Control-Flow/practice.py
+++ Python-Programming-Foundation/Python-Fundamentals/Control-Flow/practice.py
@@ -1,38 +1,66 @@
 # Prime Number Program
 
-# n=int(input("Num: "))
-# isprime=True
-# if n <= 1:
-#     print("Not Prime")
-# else:
-#     isprime = True
-# for i in range(2,n):
-#     if n%i==0:
-#         isprime=False
-#         break
-        
-# if isprime:
-#     print(" Prime")
-# else:
-#     print("NotPrime")
+n = int(input("Num: "))
 
-#    OUTPUT---------------------
+if n <= 1:
+    print("Not Prime")
+else:
+    isprime = True
 
-# Num: 7
-#  Prime
-# Num: 9
-# NotPrime
+    for i in range(2, n):
+        if n % i == 0:
+            isprime = False
+            break
+
+    if isprime:
+        print("Prime")
+    else:
+        print("Not Prime")
 
 
-n=int(input("Num"))
-n1=0
-n2=1
-if n==0:
+# Fibonacci Series
+
+n = int(input("Num: "))
+
+n1 = 0
+n2 = 1
+
+if n == 0:
     print(n1)
 else:
     for i in range(n):
-        print(n1,end=" ")
-        t=n1+n2
-        n1=n2
-        n2=t
+        print(n1, end=" ")
+        t = n1 + n2
+        n1 = n2
+        n2 = t
 
+
+# Palindrome
+
+n1 = int(input("Num: "))
+n = str(n1)
+
+if n == n[::-1]:
+    print("Palindrome")
+else:
+    print("Not")
+
+
+# Armstrong Number
+
+n = input("Num: ")
+
+temp = int(n)
+org = temp
+k = 0
+s = len(n)
+
+while temp > 0:
+    d = temp % 10
+    temp //= 10
+    k += d ** s
+
+if k == org:
+    print("Armstrong")
+else:
+    print("Not")
---
+++ Python-Programming-Foundation/Python-Fundamentals/Control-Flow/while_loops.py
@@ -0,0 +1,62 @@
+
+#Program 1: Print 1 to N
+
+# n=int(input("Num: "))
+# i=1
+# while i<=n:
+#     print(i)
+#     i+=1
+
+#    OUTPUT---------------------
+# Num: 5
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+# Program 2: Sum of First N Numbers
+
+# n=int(input("Num: "))
+# i=1
+# s=0
+# while i<=n:
+#     s+=i
+#     i+=1
+# print("Sum:",s)
+
+#    OUTPUT---------------------
+
+
+#Program 3: Reverse a Number
+
+# Num: 5
+# Sum: 15
+
+# n=int(input("Num: "))
+# d=0
+# while n>0 :
+#     r=n%10
+#     d=d*10+r
+#     n//=10
+# print(d)
+
+#    OUTPUT---------------------
+
+# Num: 1234
+# 4321
+
+#Program 4: Count Digits
+
+# n=int(input("Num: "))
+# count=0
+# while n>0:
+#     count += 1
+#     n //= 10
+# print(count)
+
+#    OUTPUT---------------------
+
+# Num: 12345
+# 5
